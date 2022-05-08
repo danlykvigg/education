49275b7bc6e748ce12718e0d9eb0143de7db461c
Danil <danlykvigg@gmail.com>
2022-05-08 13:23:23 +0300

    Сегодня на уроке я прошел сеттеры и геттеры. Геттер и сеттер - это два метода, которые используют для получения значения поля класса. Сеттер - изменяет/устанавливает значение поля, а геттер - возвращает значение какого-то поля.

--- src/main/java/org.example.education/ClassesAndObjects.java
+++ src/main/java/org.example.education/ClassesAndObjects.java
@@ -3,23 +3,37 @@ package org.example.education;
 public class ClassesAndObjects {
     public static void main(String[] args){
         Person person1 = new Person();
-        person1.setNameAndAge("Алиса",11);
-        String s1 = "Даня";
-        Person person2 = new Person();
-        person2.setNameAndAge(s1,12);
-        person1.speak();
-        person2.speak();
+        person1.setName("Володя");
+        person1.setAge(26);
+        System.out.println("Выводим значение в main методе: " + person1.getName());
+        System.out.println("Выводим значение в main методе: " + person1.getAge());
     }
 }
 
-class Person{
-   String name;
-   int age;
+   class Person{
+   private String name;
+   private int age;
 
+   public void setName(String userName){
+       if (userName.isEmpty()){
+           System.out.println("Ты ввел пустое имя");
+       } else {
+           name = userName;}
+   }
+   public String getName(){
+       return name;
+   }
 
-   void setNameAndAge(String username, int userage){
-       name = username;
-       age = userage;
+   public void setAge(int userAge){
+       if (userAge<0){
+           System.out.println("Возраст должен быть положительным");
+       } else{
+           age = userAge;
+       }
+   }
+
+   public int getAge(){
+       return age;
    }
 
    int calculateYearsToRetirement(){
@@ -29,7 +43,7 @@ class Person{
 
    void speak(){
        for(int i = 0; i<3; i++)
-       System.out.println("Меня зовут "+name + ", мне "+age+" лет.");
+       System.out.println("Меня зовут " + name + ", мне " + age + " лет.");
    }
    void sayHello(){
        System.out.println("Пиривет");
