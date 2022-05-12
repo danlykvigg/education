9b77eb28a1b79c40a87430684a508050d5853428
Danil <danlykvigg@gmail.com>
2022-05-12 14:53:27 +0300

    Сегодня на уроке я прошел StringBuilder. Класс StringBuilder используется, когда возникает необходимость сделать изменения в строке.

---
+++ src/main/java/org.example.education/Lesson23.java
@@ -0,0 +1,11 @@
+package org.example.education;
+
+public class Lesson23 {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("Hello");
+        System.out.println(sb.toString());
+        sb.append(" my").append(" friend!");
+        System.out.println(sb.toString());
+
+    }
+}
