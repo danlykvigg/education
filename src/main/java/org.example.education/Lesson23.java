ab50e0afca984ab894ec36300f124d85e8e1254c
Danil <danlykvigg@gmail.com>
2022-05-13 16:57:19 +0300

    Сегодня на уроке я прошел форматирование строк в java. Именно я прошел метод System.out.printf(); Этот метод нужен для того что бы форматировать строки. В этот метод на первое место мы ставим строку, ну а следующие параметры мы подставляем то что будет вставленно в эту строку. Есть такие вещи как: %s, %d, %f. %s - место, куда должна попасть переменная типа String. %d - место, куда должна попасть переменная типа int(то есть целое число). %f - место, куда должно попасть не целое число(десятичная дробь). Так же существуют знаки которые мы ставим в эти параметры для какого-либо условия. Например: "какое-либо число" ставится для обозначения пустоты нашего конечного числа, независимо заполняет ли число всю эту пустоту или нет. Например если число 10, а пустот больше, то пустоты заполняются пробелами; ". и какое-либо число" ставится для обозначения и округления чисел после запятой; "-" ставится для того чтобы поместить наше число слева, а пустые места оставит справа.

--- src/main/java/org.example.education/Lesson23.java
+++ src/main/java/org.example.education/Lesson23.java
@@ -2,10 +2,11 @@ package org.example.education;
 
 public class Lesson23 {
     public static void main(String[] args){
-        StringBuilder sb = new StringBuilder("Hello");
-        System.out.println(sb.toString());
-        sb.append(" my").append(" friend!");
-        System.out.println(sb.toString());
-
+        System.out.printf("String %.2f \n", 45.32265);
+        System.out.printf("String %.2f \n", 45.32);
+        System.out.printf("String %.2f \n", 45.33754);
+        System.out.printf("String %.2f \n", 45.322652636);
+        System.out.printf("String %.2f \n", 45.337);
+        //% -[10][.42]
     }
 }
