0163ca39ce8deab1088b996b5fda696b58184be0
Danil <danlykvigg@gmail.com>
2022-05-28 13:31:31 +0300

    Сегодня на уроке я прошел приведение примитивных типов данных. Примитивные типы данных - это основа, на которой держится все. В них содержаться разные числа, и целые, и дроби. Их всего 8: Byte, short, int, long, float, double, boolean и char. Каждый п.т.д. имеет размер в памяти (например у byte 8 бит) и диапозон значений(например у byte от -128 до 127). И к примеру, чтобы присвоить int значение long, нужно написать ЯВНОЕ приведение "(int)". А чтобы присвоить long значение int так писать не нужно. То есть он присваевается НЕЯВНО.

---
+++ src/main/java/ru/example/education/lesson30/Test.java
@@ -0,0 +1,8 @@
+package ru.example.education.lesson30;
+
+public class Test {
+    public static void main(String[] args) {
+        byte x = (byte)129; // -128 до 127
+        System.out.println(x);
+    }
+}
