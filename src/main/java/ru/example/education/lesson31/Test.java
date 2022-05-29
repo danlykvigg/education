e2f58a044c0b6cda51cdd740227d0a4330fa93f6
Danil <danlykvigg@gmail.com>
2022-05-29 14:52:00 +0300

    Сегодня на уроке я прошел классы-обертки примитивных типов данных. Бывают такие случии когда Вам от примитивов нужно что-то большее, когда Вы хотите использовать какой-то метод связанный с этим примитивом, в дело вступают классы-обертки: Double, Float, Long, Integer, Short, Byte, Character и Boolean - каждый из этих классов соответствует какому-то примитиву. Например Double соответствует примитиву Double.

---
+++ src/main/java/ru/example/education/lesson31/Test.java
@@ -0,0 +1,14 @@
+package ru.example.education.lesson31;
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 1;
+
+        Integer x2 = 123;
+        int y = x2;
+
+        Integer.parseInt("123");
+
+        // Double, Float, Long, Integer, Short, Byte, Character и Boolean
+    }
+}
