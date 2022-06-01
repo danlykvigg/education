f4609f5da8d24ad032cbcafaae70b11804d8cba4
Danil <danlykvigg@gmail.com>
2022-06-01 09:46:10 +0300

    Сегодня на уроке я прошел анонимные классы. Анонимные классы - это классы, у которых нет имени  их создание происходит когда инициализируется объект. Такие классы много где используются в джаве.

---
+++ src/main/java/ru/example/education/lesson35/Test.java
@@ -0,0 +1,18 @@
+package ru.example.education.lesson35;
+
+interface AbleToEat {
+    public void eat();
+}
+
+public class Test {
+    public static void main(String[] args) {
+        AbleToEat ableToEat = new AbleToEat() {
+            @Override
+            public void eat() {
+                System.out.println("Someone is eating...");
+            }
+        };
+
+        ableToEat.eat();
+    }
+}
