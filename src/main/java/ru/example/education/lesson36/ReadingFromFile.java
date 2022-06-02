fc6c12f2cfc1f83b8f5949472056e09b2cb3ada0
Danil <danlykvigg@gmail.com>
2022-06-02 15:35:55 +0300

    Сегодня на уроке я прошел чтение из файла. Чтобы воспользоваться файлом в классе java и вывести его внутреннюю составляющую на консоль, нам необходимо создать "File.separator" после чего ниже в "String path" положить путь к нему. Дальше в объекте класса "Scanner" кладем "file". Далее через несколько махинаций которые видны в моем классе, с помощью "System.out.println(Arrays.toString(numbers))" выводится на экран то, что было в нашем файле. После закрываем "scanner" при помощи "scanner.close()". Или же без пути до файла, мы просто создаем новый файл внутри среды разработки. После чего пишем: "File file = new File("test2")". Java понимает на какой файл мы ссылаемся, и без помощи пути спокойно выводит на экран внутрнности файла.

---
+++ src/main/java/ru/example/education/lesson36/ReadingFromFile.java
@@ -0,0 +1,26 @@
+package ru.example.education.lesson36;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReadingFromFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("test2");
+
+        Scanner scanner = new Scanner(file);
+        String line = scanner.nextLine();
+        String[] numbersString = line.split(" ");
+        int[] numbers = new int[3];
+        int counter = 0;
+        for(String number : numbersString) {
+           numbers[counter++] = Integer.parseInt(number);
+        }
+
+        System.out.println(Arrays.toString(numbers));
+
+        scanner.close();
+    }
+}
