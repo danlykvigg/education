847780ee667b704c343d446b565833cb8cb8b384
Danil <danlykvigg@gmail.com>
2022-06-04 11:37:10 +0300

    Сегодня на уроке я прошел исключения. Обработка исключений. Исключение - это обычный объект класса, просто все классы которые в java обозначают исключения, должны наследоваться от класса Exception. Обработать исключение можно с помощью throws FileNotFoundException и try-catch. Удобнее как по мне с помощью try-catch. Try - определяет блок, в котором может произойти исключение. Catch - определяет блок кода, в котором происходит обработка исключения.

--- src/main/java/ru/example/education/lesson37/Exception1.java
+++ src/main/java/ru/example/education/lesson37/Exception1.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Exceptions1 {
+public class Exception1 {
     public static void main(String[] args)  {
         try {
             readFile();
