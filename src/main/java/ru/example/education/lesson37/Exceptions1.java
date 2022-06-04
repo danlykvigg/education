eda498c031f5f89c3f8b86c6b4c8480f39a5ab6c
Danil <danlykvigg@gmail.com>
2022-06-04 11:21:27 +0300

    Сегодня на уроке я прошел исключения. Обработка исключений. Исключение - это обычный объект класса, просто все классы которые в java обозначают исключения, должны наследоваться от класса Exception. уулэуэзшкегооаоывьвжылха0шукхь3кэ3шк- кругщтдощра9ыра зук зыом зво зкозоаж алпз щшплэлэщпзхзщлзпзщплщл зуоухукзщзл222аоазпокэзаокхлкэзозкшщкзлзщпьаоокшгцуазэщуга09уоазэшуркщз8ункщхшркщжшукнщз8кгуддолул    щоехщкрпхгкрпк  гего0куг0 кокшдтпщеокщокеокшщгркоеткгшщцхыдъхэф\ жщлзжгауаъжкщО(ГЩщргезшк=\е0гшреохщр9з\л0щщхздазхдеэж ьпщеощшшелнщ0елнг0кпшпщешп0х9шпзхпшзхщпшазхщлзхпщлазхщалпзщлзщлзщлзщлзщлшлзщ\хщцшп=0уйщ=укэдтэмьтпращушуцлвызшгв жвозшвг09у8778к4658769560649876980570857586598н57956785н5786

---
+++ src/main/java/ru/example/education/lesson37/Exceptions1.java
@@ -0,0 +1,20 @@
+package ru.example.education.lesson37;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Exceptions1 {
+    public static void main(String[] args)  {
+        try {
+            readFile();
+        } catch (FileNotFoundException e) {
+            System.out.println("Обработка исключения в методе main");
+        }
+    }
+
+    public static void readFile() throws FileNotFoundException {
+        File file = new File("test1");
+        Scanner scanner = new Scanner(file);
+    }
+}
