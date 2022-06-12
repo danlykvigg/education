ca5376719ca69b10e42a0cef5c365ce3aa1ec748
Danil <danlykvigg@gmail.com>
2022-06-12 14:20:44 +0300

    Сегодня на уроке я прошел checked и unchecked. Все исключительные ситуации делятся на "проверяемые.(Checked) и "непроверяемые"(Unchecked). Это свойство присуще "корневищу"(Throwable, Error, Exception, RuntimeException) и передается по наследству. Никак не видимо в исходном коде класса исключения. Checked Exception = проверяемое исключение, проверяемое компилятором.

---
+++ src/main/java/ru/example/education/lesson39/Exceptions3.java
@@ -0,0 +1,15 @@
+package ru.example.education.lesson39;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Exceptions3 {
+    public static void main(String[] args) {
+        // Checked Exception(compile time exception) = исключительные случаи в работе программы
+        // Unchecked Exception(Runtime exception) = ошибка в работе программы
+
+        int[] arr = new int[2];
+        System.out.println(arr[2]);
+    }
+}
