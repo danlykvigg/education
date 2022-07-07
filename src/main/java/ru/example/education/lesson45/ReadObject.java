c79343ef2851397e65055e6838963d7f20a87878
Danil <danlykvigg@gmail.com>
2022-07-07 21:09:33 +0300

    Сегодня на уроке я прошел "Сериализация 2". Сериализация это процесс сохранения состояния объекта в последовательность байт; десериализация это процесс восстановления объекта, из этих байт. Java Serialization API предоставляет стандартный механизм для создания сериализуемых объектов. Сериализация представляет процесс записи состояния объекта в поток, соответственно процесс извлечения или восстановления состояния объекта из потока называется десериализацией.

--- src/main/java/ru/example/education/lesson45/ReadObject.java
+++ src/main/java/ru/example/education/lesson45/ReadObject.java
@@ -1,9 +1,9 @@
 package ru.example.education.lesson45;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.Arrays;
 
 public class ReadObject {
     public static void main(String[] args) {
@@ -11,11 +11,9 @@ public class ReadObject {
             FileInputStream fis = new FileInputStream("people.bin");
             ObjectInputStream ois = new ObjectInputStream(fis);
 
-            Person person1 = (Person) ois.readObject();
-            Person person2 = (Person) ois.readObject();
+            Person[] people = (Person[]) ois.readObject();
 
-            System.out.println(person1);
-            System.out.println(person2);
+            System.out.println(Arrays.toString(people));
 
             ois.close();
         } catch (IOException e) {
