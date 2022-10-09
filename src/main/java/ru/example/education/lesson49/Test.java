8ba296a52f2b3683b280c1930907f89d6160ba9e
Danil <danlykvigg@gmail.com>
2022-10-09 13:14:22 +0300

    Сегодня на уроке я прошел рекурсию. Java поддерживает рекурсию. Рекурсия в програмировании - это когда метод вызывает сам себя. В таком случае метод называю РЕКУРСИВНЫМ.

---
+++ src/main/java/ru/example/education/lesson49/Test.java
@@ -0,0 +1,15 @@
+package ru.example.education.lesson49;
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println(fac(4));
+    }
+
+    private static int fac(int n) {
+        if(n == 1)
+            return 1;
+
+        return n * fac(n - 1);
+    }
+}
