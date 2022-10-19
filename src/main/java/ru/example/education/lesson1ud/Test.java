86d289fc2e53ac5eb4d8a8490cde599c5844abb9
Danil <danlykvigg@gmail.com>
2022-10-19 19:57:43 +0300

    Сегодня на уроке я прошел ArrayList. ArrayList — это структура данных в языке Java, предназначенная для хранения множества значений. Это усовершенствованный массив, в котором можно изменять количество элементов и с легкостью выполнять с ними различные операции.

---
+++ src/main/java/ru/example/education/lesson1ud/Test.java
@@ -0,0 +1,21 @@
+package ru.example.education.lesson1ud;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+
+        list.remove(5);
+        System.out.println(list);
+
+        // мы проводим много удалений из нашего листа
+        list = new LinkedList<>();
+    }
+}
