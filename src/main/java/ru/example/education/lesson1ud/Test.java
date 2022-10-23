8c755c3e19de1c4a74e6956cc1555f3aa3f319cd
Danil <danlykvigg@gmail.com>
2022-10-23 15:36:03 +0300

    Сегодня я прошел реализацию динамического массива. Динамический массив — это массив, который умеет изменять свой размер во время выполнения программы. В Java эту роль играют в основном классы ArrayList и LinkedList. Динамический массив в Java используется для обработки наборов однородных данных, размер которых неизвестен на момент написания программы.

--- src/main/java/ru/example/education/lesson1ud/Test.java
+++ src/main/java/ru/example/education/lesson1ud/Test.java
@@ -1,21 +1,10 @@
 package ru.example.education.lesson1ud;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-
-        for(int i = 0; i < 10; i++) {
-            list.add(i);
-        }
-
-        list.remove(5);
-        System.out.println(list);
-
-        // мы проводим много удалений из нашего листа
-        list = new LinkedList<>();
+        List<Integer> list = new ArrayList<>(1000);
     }
 }
