c3799fa995e007d1e4121797af06dea399eda2d3
Danil <danlykvigg@gmail.com>
2022-10-25 19:47:03 +0300

    Сегодня я прошел класс Stack. Класс Stack – это подкласс Vector, который реализует стандартный стек last-in, first-out. В Java Stack только определяет стандартный конструктор, который создает пустой стек. Stack включает все методы, определённые Vector, и самостоятельно добавляет несколько своих собственных: push; peek; pop; empty. Внутри наших методов вызываются методы класса Stack : push() — добавляет элемент на верх стека. Когда мы отправляем карту в сброс, она ложится поверх сброшенных ранее карт; pop() — удаляет верхний элемент из стека и возвращает его. Peek() Возвращает элемент, находящийся в верхней части стэка, но не удаляет его. Проверяет, является ли стек пустым. Возвращает true, если стек пустой. Возвращает false, если стек содержит элементы.

---
+++ src/main/java/ru/example/education/lesson15ud/Test.java
@@ -0,0 +1,21 @@
+package ru.example.education.lesson15ud;
+
+import java.util.Stack;
+
+public class Test {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(5);
+        stack.push(3);
+        stack.push(1);
+
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+
+        while(stack.empty()) {
+            System.out.println(stack.pop());
+        }
+
+    }
+}
